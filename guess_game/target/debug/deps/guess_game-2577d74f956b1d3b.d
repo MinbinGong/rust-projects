a4dcae2a1e47fb0db1b1f4d1f7463c5751908ee0
Wayne Gong <gongwayne@hotmail.com>
2019-10-22 16:28:17 +0800

    Guess game

---
+++ guess_game/target/debug/deps/guess_game-2577d74f956b1d3b.d
@@ -0,0 +1,5 @@
+/home/wayne/workspace/rust-projects/guess_game/target/debug/deps/guess_game-2577d74f956b1d3b: src/main.rs
+
+/home/wayne/workspace/rust-projects/guess_game/target/debug/deps/guess_game-2577d74f956b1d3b.d: src/main.rs
+
+src/main.rs:
---
+++ guess_game/target/debug/guess_game.d
@@ -0,0 +1,1 @@
+/home/wayne/workspace/rust-projects/guess_game/target/debug/guess_game: /home/wayne/workspace/rust-projects/guess_game/src/main.rs
