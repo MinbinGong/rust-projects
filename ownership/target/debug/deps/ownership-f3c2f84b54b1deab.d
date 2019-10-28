a8f27a7045ce9c4019642a054464613b8d2a46a3
Wayne Gong <minbgong@cisco.com>
2019-10-28 15:32:58 +0800

    chapter four

---
+++ ownership/target/debug/deps/ownership-f3c2f84b54b1deab.d
@@ -0,0 +1,5 @@
+D:\Workspace\GitRepos\rust-projects\ownership\target\debug\deps\ownership-f3c2f84b54b1deab.exe: src\main.rs
+
+D:\Workspace\GitRepos\rust-projects\ownership\target\debug\deps\ownership-f3c2f84b54b1deab.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+D:\Workspace\GitRepos\rust-projects\ownership\target\debug\ownership.exe: D:\Workspace\GitRepos\rust-projects\ownership\src\main.rs
